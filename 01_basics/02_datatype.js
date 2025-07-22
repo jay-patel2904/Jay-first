75039887a3cf6186c240b9a273f8b3ebac8a2e16
jay-patel2904 <jayelectricalservices2701@gmail.com>
2025-07-22 06:16:19 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as a new version
+
+// alert( 3+3 ) // we are using nodejs not broswer
+console.log( 3+3 ); 
+
+console.log("jay");
+
+// code readability should be high
+
+let name = "Jay"
+let age = 18
+let loggedIn = true
+let state = null
+let village; 
+
+// number => 2 to power
+// bigint 
+// stirng = ""
+// boolean => true/false
+// null => standalone value
+// undifined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof village);
+
+
+console.table(["Jay", age, loggedIn, village, state])
+
+console.log(typeof null); // object
+
+console.log(undefined); // undefined
+
