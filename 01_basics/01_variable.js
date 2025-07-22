efb891e83d6a2aee0454f9f0bc9e7401daf91365
jay-patel2904 <jayelectricalservices2701@gmail.com>
2025-07-22 06:14:31 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 456123
+let accountEmail = "jay@jay.com"
+var accountPassword = "12345"
+accountCity = "ahmedabad"
+let accountState;
+
+accountPassword = "45612"
+
+console.log(accountPassword);
+
+
+// accountId = 789456 // not allowd bcz it's const (constant)
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* 
+prefer not to use var 
+beacuse of issues in block scope and functional scope
+*/
